3d4876997a415eb5ed68099324ab4f7d3b16d0fe
Noah Pikielny <npikielny@gmail.com>
2021-06-15 21:19:19 -0400

    3D Rendering

---
+++ GraphicsHub/Renderer/3DRendering/DataTypes.swift
@@ -0,0 +1,22 @@
+//
+//  DataTypes.swift
+//  GraphicsHub
+//
+//  Created by Noah Pikielny on 6/14/21.
+//
+
+import Foundation
+
+struct Camera {
+    var position: SIMD3<Float>
+    var rotation: SIMD3<Float>
+    var fov: Float
+}
+
+struct Material {
+    
+}
+
+struct Scene {
+    
+}
